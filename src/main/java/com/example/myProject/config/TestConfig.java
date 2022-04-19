5a0cdd8bb85beb987dcac242f1bbdd8dcf1ee695
dennis-castro <d3nnis_castro@hotmail.com>
2022-04-19 00:37:04 -0300

    Payment, one-to-one association

--- src/main/java/com/example/myProject/config/TestConfig.java
+++ src/main/java/com/example/myProject/config/TestConfig.java
@@ -73,5 +73,12 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
+        orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
+        Payment pay1 = new Payment(null,Instant.parse("2019-07-21T21:42 :10Z"), o1 );
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 }
